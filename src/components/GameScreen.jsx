69c4c7be0f22b6a65be8886b11ace1949d930954
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2023-06-20 21:12:40 -0300

    GameScreen Front

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -4,9 +4,31 @@ import './GameScreen.css'
 
 export const GameScreen = ({ click }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={ click }>Finalizar Jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <p>Você ainda tem XXX tentativas</p>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"> </span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form action="">
+          <input type="text" name='letter' maxLength='1'required/>
+            <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersCotnainer">
+        <p>Letras erradas:</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   )
 }
